652feab9b840e5163df30f29d946e7d60f881615
Sultan Abd-Elkareem Sultan <sultanelsultan4@gmail.com>
2026-03-27 10:36:43 +0200

    Update settings.py

--- sinkrow-hotspot/sinkrow_hotspot/settings.py
+++ sinkrow-hotspot/sinkrow_hotspot/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-2nlj#froq4qb&6cdvj%w_3%v)q30t5o@k$c(i$l#tf*wg*s(gq'
+SECRET_KEY = 'demosecret'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -87,9 +87,9 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'radius',
         'USER': 'radius',
-        'PASSWORD': 'radius_password',
+        'PASSWORD': 'demosecret',
         'HOST': '127.0.0.1',
-        'PORT': '3306'
+        'PORT': '54554'
     }
 }
 
